fd51f4f960af4f80ddc6da1c3e6c63b75d5fa228
kirin <rudenokaleksandr21@gmail.com>
2024-10-15 08:08:26 +0000

    update main

--- main.go
+++ main.go
@@ -3,13 +3,26 @@ package main
 import (
 	"flag"
 	"log"
+	tgClient "tg_alarm_bot/client/telegram"
+	event_consumer "tg_alarm_bot/consumer/event-consumer"
+	"tg_alarm_bot/events/telegram"
 )
 
 const (
 	tgBotHost = "api.telegram.org"
+	batchSize = 100
 )
 
-func main() {}
+func main() {
+	eventProcessor := telegram.New(tgClient.New(tgBotHost, mustToken()))
+
+	log.Printf("service started")
+
+	consumer := event_consumer.New(eventProcessor, eventProcessor, batchSize)
+	if err := consumer.Start(); err != nil {
+		log.Fatal(err)
+	}
+}
 
 // mustToken parses the token flag from the command-line arguments.
 // If the token flag ("-t") is not specified, the function logs a fatal error and exits the program.
